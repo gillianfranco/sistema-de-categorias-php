9741b90dd09d7556cc5e3de031b563e05f0a1b94
Gillian <gillian.melo.franco17@gmail.com>
2025-07-08 11:42:26 -0300

    feat: criar tabelas assim que a página é carregada pela primeira vez

--- src/Controllers/Produtos.php
+++ src/Controllers/Produtos.php
@@ -7,9 +7,6 @@
     $id_category = $_POST['id-categoria'];
 
     if (!empty($product_name) && !empty($id_category)){
-        if (!table_exists($connection, $table_products)){
-            create_product_table($connection, $table_products);
-        }
 
         register_produtct($connection, $table_products, $product_name, $amount, $id_category);
     }
--- src/Database/db-connection.php
+++ src/Database/db-connection.php
@@ -1,4 +1,4 @@
-<?php
+<?php    
     function db_exists($conn, $db_name){
         $sql = "SHOW DATABASES LIKE '$db_name'";
         $result = mysqli_query($conn, $sql);
@@ -19,8 +19,49 @@
         }
     }
 
+    function table_exists($conn, $table_name) {
+        $sql = "SHOW TABLES LIKE '$table_name'";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            return mysqli_num_rows($result) > 0;
+        }
+
+        return false;
+    }
+
+    function create_category_table($conn, $table_name) {
+        $sql = "CREATE TABLE `$table_name` (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        qtde_produtos INT
+        )";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (!$result) {
+            die("Erro ao criar a tabela: " . mysqli_error($conn));
+        }
+    }
+
+    function create_product_table($conn, $table_name){
+        $sql = "CREATE TABLE `$table_name` (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        nome VARCHAR(100) NOT NULL,
+        unidades INT,
+        id_categoria INT
+        )";
+
+        $result = mysqli_query($conn, $sql);
+
+        if (!$result) {
+            die("Erro ao criar a tabela: " . mysqli_error($conn));
+        }
+    }
+
     // Conexão com o MySQL
-    $connection = mysqli_connect("localhost", "root", "");
+    $credentials = require_once "db-credentials.php";
+    $connection = mysqli_connect($credentials['host'], $credentials['user'], $credentials['password']);
 
     if(!$connection) {
         die("A conexão falhou: " . mysqli_connect_error());
@@ -36,8 +77,16 @@
 
     mysqli_select_db($connection, $db_name); // Seleciona o banco de dados para as próximas consultas
 
-    // Nome das tabelas
+    // Criação das tabelas
 
     $table_categories = "categorias";
     $table_products = "produtos";
+
+    if (!table_exists($connection, $table_categories)){
+        create_category_table($connection, $table_categories);
+    }
+    
+    if (!table_exists($connection, $table_products)) {
+        create_product_table($connection, $table_products, TRUE);
+    }
 ?>
